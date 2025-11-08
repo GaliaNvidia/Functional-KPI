b054bfe71ad8ec2f1fd909c23e65ac0bf9be0d0d
GaliaNvidia <galiaf@nvidia.com>
2025-11-08 16:47:16 +0200

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 👇 חשוב: החליפי Functional-KPI בשם הריפו שלך ב-GitHub אם שונה
 export default defineConfig({
+  base: '/Functional-KPI/',
   plugins: [react()],
 })
-
